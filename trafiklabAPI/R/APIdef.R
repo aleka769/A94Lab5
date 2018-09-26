599313a67b6fcb806d39f649b879508c41ec1253
Napoleongl <guslu389@student.liu.se>
2018-09-26 11:29:50 +0200

    linucx address working

--- trafiklabAPI/R/APIdef.R
+++ trafiklabAPI/R/APIdef.R
@@ -4,4 +4,6 @@ querylist <- list(key = "045ab36b-e959-41c2-8b47-49caecc6d039",
                   originCoordLong = 18.083519)
 a<-httr::GET(url = "https://api.resrobot.se/v2/location.nearbystops.json?",
              query = querylist)
-
+b<- jsonlite::fromJSON(txt= "https://api.resrobot.se/v2/location.nearbystops.json?key=045ab36b-e959-41c2-8b47-49caecc6d039&originCoordLat=58.397&originCoordLong=15.576")
+b$StopLocation
+ 
