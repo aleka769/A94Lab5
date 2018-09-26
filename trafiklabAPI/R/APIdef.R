c2bfc591a3c81ec6ac4b996a3475228c7766680f
Napoleongl <guslu389@student.liu.se>
2018-09-26 11:04:23 +0200

    got it to work in linux

--- trafiklabAPI/R/APIdef.R
+++ trafiklabAPI/R/APIdef.R
@@ -1,12 +1,7 @@
-pre_key = "https://api.resrobot.se/v2/location.nearbystops?key="
-API_key = "045ab36b-e959-41c2-8b47-49caecc6d039"
-post_key <- "&originCoordLat=59.329630&originCoordLong=18.059338&format=json"
-
-
-httr::GET(url = "https://api.resrobot.se/v2/location.nearbystops.json?key=<KEY>&originCoordLat=59.293525&originCoordLong=18.083519")
-
-url <- paste0(pre_key, API_key, post_key)
-res <- curl::curl_fetch_memory(url, handle = curl::new_handle(nobody=T))
-curl::parse_headers_list(res$headers)
-
+library(httr)
+querylist <- list(key = "045ab36b-e959-41c2-8b47-49caecc6d039",
+                  originCoordLat = 59.293525,
+                  originCoordLong = 18.083519)
+a<-httr::GET(url = "https://api.resrobot.se/v2/location.nearbystops.json?",
+             query = querylist)
 
