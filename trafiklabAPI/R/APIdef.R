bf0ed9f8bd3399a96999d8d713e153cd8c64ea02
Napoleongl <guslu389@student.liu.se>
2018-09-26 22:23:17 +0200

    added api test calls

--- trafiklabAPI/R/APIdef.R
+++ trafiklabAPI/R/APIdef.R
@@ -1,9 +1,27 @@
 library(httr)
-querylist <- list(key = "045ab36b-e959-41c2-8b47-49caecc6d039",
-                  originCoordLat = 59.293525,
-                  originCoordLong = 18.083519)
-a<-httr::GET(url = "https://api.resrobot.se/v2/location.nearbystops.json?",
-             query = querylist)
-b<- jsonlite::fromJSON(txt= "https://api.resrobot.se/v2/location.nearbystops.json?key=045ab36b-e959-41c2-8b47-49caecc6d039&originCoordLat=58.397&originCoordLong=15.576")
-b$StopLocation
+library(jsonlite)
+api_url <- "https://api.resrobot.se/v2/location.nearbystops.json?"
+api_key <- "045ab36b-e959-41c2-8b47-49caecc6d039"
+liu_nkpg <- list(key = api_key,
+                 originCoordLat = 59.591,
+                 originCoordLong = 16.178)
+
+liu_nkpg_result <- jsonlite::fromJSON(httr::modify_url(url = api_url, 
+                                                       query = liu_nkpg))
+
+liu_lkpg <- list(key = api_key,
+                 originCoordLat = 58.399,
+                 originCoordLong = 15.583)
+
+liu_lkpg_result <- jsonlite::fromJSON(httr::modify_url(url = api_url, 
+                                                       query = liu_lkpg))
+
+liu_us <- list(key = api_key,
+               originCoordLat = 58.400,
+               originCoordLong = 15.620)
+
+liu_us_result <- jsonlite::fromJSON(httr::modify_url(url = api_url, 
+                                                     query = liu_us))
+
+
  
