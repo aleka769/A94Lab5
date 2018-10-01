a190dfad6540e4a896f2822888ed4b701af9fcab
Alexander Karlsson <aleka769@li17-9.ida.liu.se>
2018-10-01 15:46:18 +0200

    removed really weird lines of code, from linux error on guslu's computer???

--- trafiklabAPI/tests/testthat.R
+++ trafiklabAPI/tests/testthat.R
@@ -1,13 +1,4 @@
 library(testthat)
 library(trafiklabAPI)
 
-<<<<<<< HEAD
-test_that(desc = "Non-numeric input for longitude/latitude", {
-  code = expect_error(nearby_stops( 16.178,  "58.591", api_key = api_key))
-  code = expect_error(nearby_stops("16.178",  58.591,  api_key = api_key))
-  code = expect_error(nearby_stops("16.178", "58.591", api_key = api_key))
-})
-=======
->>>>>>> 5a5de848ff34ecd68649eb16fbadc34f31b61307
-
 test_check("trafiklabAPI")
